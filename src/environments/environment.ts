02b1e2523029b9ecbd011c7403d11856406acbf4
Zulqurnain <33602118+Zukqurnain@users.noreply.github.com>
2020-06-13 22:17:46 +0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: true,
   firebase : {
-  apiKey: "AIzaSyDCgYqRqxq3ap2YHEeKt_gEdbzOCaz3ZiE",
-  authDomain: "auth-login1.firebaseapp.com",
-  databaseURL: "https://auth-login1.firebaseio.com",
-  projectId: "auth-login1",
-  storageBucket: "auth-login1.appspot.com",
-  messagingSenderId: "347223375012"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ""
 },
 };
 
